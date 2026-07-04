09233e4a87cb37b9fcbda9195b36164a26628894
ankitjain0748 <ankitjain0748@gmail.com>
2026-07-04 18:02:34 +0530

    add forent

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,29 +4,53 @@ export default function Document() {
   return (
     <Html lang="en-IN">
       <Head>
+        {/* Google Tag Manager - Head Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5JPZS77S');`,
+          }}
+        />
+
         {/* Character Encoding */}
         <meta charSet="UTF-8" />
 
-        {/* Theme */}
+        {/* Theme Color */}
         <meta name="theme-color" content="#00BFFF" />
+
+        {/* Brand/Company Meta Tags for Global Reach */}
+        <meta name="author" content="Seankrix Technologies" />
+        <meta name="robots" content="index, follow" />
 
         {/* Icons */}
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
 
-        {/* Fonts */}
+        {/* Fonts & Performance Optimization */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
         />
-
         <link rel="dns-prefetch" href="//fonts.googleapis.com" />
       </Head>
 
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) - Crucial for tracking if JS is disabled */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5JPZS77S"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <Main />
         <NextScript />
       </body>
